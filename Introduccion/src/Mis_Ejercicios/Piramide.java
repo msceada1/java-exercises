fe2778e90d8be93601f99f8e2ebe13358b98e216
msceada1 <195111856+msceada1@users.noreply.github.com.>
2025-09-16 21:47:21 +0200

    nuevo ejercicio

---
+++ Introduccion/src/Mis_Ejercicios/Piramide.java
@@ -0,0 +1,20 @@
+package Mis_Ejercicios;
+
+import java.util.Arrays;
+
+public class Piramide {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+
+            System.out.println(" ");
+
+            for (int j = 0; j < i + 1; j++) {
+
+                System.out.print(" ");
+
+                System.out.print("*");
+            }
+        }
+    }
+}
