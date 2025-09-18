5db4a0d68d8aa3f8d47e8c4e0182063d4adebc45
msceada1 <195111856+msceada1@users.noreply.github.com.>
2025-09-18 09:22:52 +0200

    optimizando piramide

--- Introduccion/src/Mis_Ejercicios/Piramide.java
+++ Introduccion/src/Mis_Ejercicios/Piramide.java
@@ -4,19 +4,14 @@ public class Piramide {
     public static void main(String[] args) {
 
         for (int i = 0; i < 4; i++) {
-
             System.out.println(" ");
 
-            for (int j = 0; j < 4 - i - 1; j++) {
-
+            for (int j = 0; j < 4 - i; j++) {
                 System.out.print(" ");
-
             }
 
-            for (int k = 0; k < 1 * i + 1; k++) {
-
+            for (int k = 0; k < 1 + i; k++) {
                 System.out.print("* ");
-
             }
         }
     }
