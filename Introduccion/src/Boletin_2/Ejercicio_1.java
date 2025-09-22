4878a19bc614b4e10d35364d9ebc066e4d239726
msceada1 <195111856+msceada1@users.noreply.github.com.>
2025-09-22 20:36:52 +0200

    nuevo boletin

---
+++ Introduccion/src/Boletin_2/Ejercicio_1.java
@@ -0,0 +1,18 @@
+package Boletin_2;
+
+import utils.MiEntradaSalida;
+
+public class Ejercicio_1 {
+
+    /*
+    Realizar un programa que, dado un número introducido por el usuario, calcule su
+    número a la inversa. Es decir, dada la entrada: 123456, el programa debe devolver
+    654321.
+     */
+
+    public static void main(String[] args) {
+
+        int numero = MiEntradaSalida.solicitarEnteroPositivo("introduce un numero");
+
+    }
+}
