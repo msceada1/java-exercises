5911d5ee5a1d3f5510cabbdda5fa94c8c84f59cd
msceada1 <195111856+msceada1@users.noreply.github.com>
2025-09-23 12:51:06 +0200

    nuevo boletin

---
+++ Introduccion/src/Boletin_3/Ejercicio_1.java
@@ -0,0 +1,27 @@
+package Boletin_3;
+
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+
+        int contadorMultiplos7 = 0, contadorMultiplos13 = 0, contadorMultiplos7Y13 = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (7 % i == 0) {
+                contadorMultiplos7++;
+            }
+            if (13 % i == 0) {
+                contadorMultiplos13++;
+            }
+            if (7 % i == 0 && 13 % i == 0) {
+                contadorMultiplos7Y13++;
+            }
+
+        }
+        System.out.println("contadorMultiplos7 = " + contadorMultiplos7);
+
+        System.out.println("contadorMultiplos13 = " + contadorMultiplos13);
+
+        System.out.println("contadorMultiplos7Y13 = " + contadorMultiplos7Y13);
+    }
+}
