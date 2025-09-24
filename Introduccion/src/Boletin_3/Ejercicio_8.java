b109757d984c2b7a2cc0875e07646ca21ca5f517
msceada1 <195111856+msceada1@users.noreply.github.com>
2025-09-24 10:14:41 +0200

    modificando ejercicios

---
+++ Introduccion/src/Boletin_3/Ejercicio_8.java
@@ -0,0 +1,23 @@
+package Boletin_3;
+
+import java.util.Scanner;
+
+public class Ejercicio_8 {
+
+    /*
+        Realizar un programa que solicite un conjunto de números. Después de
+        introducir cada número se realizará la pregunta: "¿Desea introducir más
+        números (S/N)". Si la respuesta es 'S' se solicitará otro número. Cuando no
+        desee introducir más números debe informar cual es el menor de los
+        números introducidos.
+     */
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        char respuesta1 = 'S';
+        char respuesta2 = 'N';
+
+
+    }
+}
