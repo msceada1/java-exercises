1532098b8dee50cf276d660c91ad3af83bc62a0c
msceada1 <195111856+msceada1@users.noreply.github.com.>
2025-10-13 18:56:50 +0200

    terminando ejercicio 9

--- Introduccion/src/Boletin_Arrays/Ejercicio9.java
+++ Introduccion/src/Boletin_Arrays/Ejercicio9.java
@@ -1,21 +1,36 @@
 package Boletin_Arrays;
 
+import utils.MiEntradaSalida;
+
 import java.util.Arrays;
 
 public class Ejercicio9 {
 
     public static void main(String[] args) {
 
+        int[] array = {9, 1, 4, 2, 12, 6, 8};
+        int opcion = MiEntradaSalida.leerEnteroEnRango("Opcion 1: Orden ascendente. Opcion 2: Orden descendente.", 1, 2);
+
+        System.out.println(Arrays.toString(arrayOrdenado(array, opcion)));
     }
-/*
+
     private static int[] arrayOrdenado(int[] array, int opcion) {
 
         if (opcion == 1) {
             Arrays.sort(array);
         } else if (opcion == 2) {
-
+            for (int i = 0; i < array.length - 1; i++) {
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[i] < array[j]) {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
         }
+
+        return array;
     }
-    */
 
 }
