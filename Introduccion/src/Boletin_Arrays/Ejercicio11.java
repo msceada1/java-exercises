782d404662f77955dfa400611eec478def1bd997
msceada1 <195111856+msceada1@users.noreply.github.com>
2025-10-16 13:38:28 +0200

    pensanso cambio

---
+++ Introduccion/src/Boletin_Arrays/Ejercicio11.java
@@ -0,0 +1,34 @@
+package Boletin_Arrays;
+
+import java.util.Arrays;
+
+public class Ejercicio11 {
+
+    /*
+    Crea un método que acepte 2 arrays, y devuelva un nuevo array con los elementos
+    que ambos arrays tienen en común.
+     */
+
+    public static void main(String[] args) {
+
+        int[] array1 = {1, 2, 3, 3, 2, 1, 7, 7, 4, 5, 7, 4, 14};
+        int[] array2 = {4, 5, 2, 10, 12, 6, 7, 2, 5, 6, 12, 16};
+
+
+    }
+/*
+    private static int[] devuelveElementosComunes(int[] array1, int[] array2) {
+
+        int[] arrayComun;
+
+        if (array1.length > array2.length) {
+            arrayComun = new int[array1.length];
+        } else if (array1.length == array2.length) {
+            arrayComun = new int[array1.length];
+        } else arrayComun = new int[array2.length];
+
+
+    }
+
+ */
+}
