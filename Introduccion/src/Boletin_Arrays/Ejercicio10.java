e51688b8996bd8f43ab0a2173f2e5669ab3b15f4
msceada1 <195111856+msceada1@users.noreply.github.com.>
2025-10-16 10:42:23 +0200

    terminando ejercicio 10

--- Introduccion/src/Boletin_Arrays/Ejercicio10.java
+++ Introduccion/src/Boletin_Arrays/Ejercicio10.java
@@ -10,17 +10,28 @@ public class Ejercicio10 {
         System.out.println(Arrays.toString(eliminaDuplicados(array)));
     }
 
+    /**
+     * Metodo para eliminar los elementos duplicados de un array. Creamos un array copia igual que el original.
+     * Se utiliza doble for para recorrer los array y una variable boolean que indica si el elemento dentro del array
+     * es repetido. Si el elemento no es repetido, se introduce en un array que se devuelve.
+     *
+     * @param array El array que se analiza
+     * @return el array sin elementos duplicados.
+     */
     private static int[] eliminaDuplicados(int[] array) {
 
         int[] arraySinDuplicados = new int[array.length];
         int[] arrayCopia = array;
-        int cuentaDuplicados = 0;
 
         for (int i = 0; i < array.length - 1; i++) {
+            boolean repetido = false;                       //la variable boolean me indica si el elemento es repetido
             for (int j = i + 1; j < arrayCopia.length; j++) {
-                if (array[i] != arrayCopia[j]) {
-                    arraySinDuplicados[i] = array[i];
+                if (array[i] == arrayCopia[j]) {
+                    repetido = true;
                 }
+            }
+            if (!repetido) {                               //si no ha sido repetido, entonces la copio dentro del array
+                arraySinDuplicados[i] = array[i];
             }
         }
 
