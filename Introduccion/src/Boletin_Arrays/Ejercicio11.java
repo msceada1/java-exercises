af5324a3e0ae784eea4211ea18cb29984589e964
msceada1 <195111856+msceada1@users.noreply.github.com>
2025-10-21 12:41:14 +0200

    mejorando el ejercicio 11

--- Introduccion/src/Boletin_Arrays/Ejercicio11.java
+++ Introduccion/src/Boletin_Arrays/Ejercicio11.java
@@ -17,19 +17,18 @@ public class Ejercicio11 {
         System.out.println(Arrays.toString(arrayComun(array1, array2)));
     }
 
-    private static boolean posicionComun(int[] array1, int[] array2) {
-
-        for (int i = 0; i < array1.length; i++) {
-            for (int j = 0; j < array2.length; j++) {
-                if (array1[i] == array2[j]) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
+    /**
+     * Verifica si un arreglo contiene un elemento específico.
+     *
+     * <p>Este método recorre el arreglo dado y comprueba si alguno de sus
+     * elementos coincide con el valor especificado. Si lo encuentra, devuelve {@code true};
+     * de lo contrario, devuelve {@code false}.</p>
+     *
+     * @param array    el arreglo en el que se buscará el elemento
+     * @param elemento el valor que se desea verificar si está presente en el arreglo
+     * @return {@code true} si el elemento está presente en el arreglo,
+     * {@code false} en caso contrario
+     */
     private static boolean contieneElemento(int[] array, int elemento) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == elemento) {
@@ -40,19 +39,32 @@ public class Ejercicio11 {
         return false;
     }
 
+    /**
+     * Obtiene un nuevo arreglo que contiene los elementos comunes entre dos arreglos.
+     *
+     * <p>El método busca los valores que aparecen en ambos arreglos sin repetir elementos.
+     * La longitud del arreglo resultante será igual al número de elementos en común.</p>
+     *
+     * @param array1 el primer arreglo de enteros
+     * @param array2 el segundo arreglo de enteros
+     * @return un nuevo arreglo que contiene los elementos comunes a ambos arreglos,
+     * sin duplicados
+     */
     private static int[] arrayComun(int[] array1, int[] array2) {
 
+        int min = Math.min(array1.length, array2.length);
         int contador = 0;
-        int[] arrayFusion = new int[contador];
+        int[] arrayFusion = new int[min];
 
         for (int i = 0; i < array1.length; i++) {
-            for (int j = 0; j < array2.length; j++) {
-                if (posicionComun(array1, array2)) {
-                    arrayFusion[contador++] = array1[i];
-                }
+            if (contieneElemento(array2, array1[i]) && !contieneElemento(arrayFusion, array1[i])) {
+                arrayFusion[contador++] = array1[i];
             }
         }
 
+
         return Arrays.copyOf(arrayFusion, contador);
     }
+
+
 }
